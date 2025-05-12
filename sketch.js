1739125401e0ca27ed48de937452725b406ee0d1
Pengiii18 <ab198035@gmail.com>
2025-05-12 09:59:26 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,84 +1,42 @@
-// Face Mesh Texture Mapping
-// https://thecodingtrain.com/tracks/ml5js-beginners-guide/ml5/facemesh
-// https://youtu.be/R5UZsIwPbJA
-
+let facemesh;
 let video;
-let faceMesh;
-let faces = [];
-let triangles;
-let uvCoords;
-let img;
-
-function preload() {
-  // Initialize FaceMesh model with a maximum of one face
-  faceMesh = ml5.faceMesh({ maxFaces: 1 });
-
-  // Load the texture image that will be mapped onto the face mesh
-  img = loadImage("Zombie_Dan_mesh_map.png");
-}
-
-function mousePressed() {
-  // Log detected face data to the console
-  console.log(faces);
-}
-
-function gotFaces(results) {
-  faces = results;
-}
+let predictions = [];
+const points = [409, 270, 269, 267, 0, 37, 39, 40, 185, 61, 146, 91, 181, 84, 17, 314, 405, 321, 375, 291];
 
 function setup() {
-  createCanvas(640, 480, WEBGL);
+  createCanvas(400, 400);
   video = createCapture(VIDEO);
+  video.size(400, 400);
   video.hide();
 
-  // Start detecting faces
-  faceMesh.detectStart(video, gotFaces);
+  facemesh = ml5.facemesh(video, modelReady);
+  facemesh.on("predict", results => {
+    predictions = results;
+  });
+}
 
-  // Retrieve face mesh triangles and UV coordinates
-  triangles = faceMesh.getTriangles();
-  uvCoords = faceMesh.getUVCoords();
+function modelReady() {
+  console.log("Facemesh model loaded!");
 }
 
 function draw() {
-  // Center the 3D space to align with the canvas
-  translate(-width / 2, -height / 2);
-  background(0);
+  background(220);
+  image(video, 0, 0, width, height);
+  drawFaceMesh();
+}
 
-  // Display the video feed
-  image(video, 0, 0);
+function drawFaceMesh() {
+  stroke(0, 0, 255); // 藍色線條
+  strokeWeight(5); // 線條粗細為5
+  noFill();
 
-  if (faces.length > 0) {
-    let face = faces[0];
-
-    // Apply texture mapping to the detected face mesh
-    texture(img);
-    textureMode(NORMAL);
-    noStroke();
-    beginShape(TRIANGLES);
-
-    // Loop through each triangle in the face mesh
-    for (let i = 0; i < triangles.length; i++) {
-      let tri = triangles[i];
-
-      // Get the indices of the three points that form a triangle
-      let [a, b, c] = tri;
-
-      // Retrieve the corresponding 2D face keypoints
-      let pointA = face.keypoints[a];
-      let pointB = face.keypoints[b];
-      let pointC = face.keypoints[c];
-
-      // Retrieve the corresponding UV coordinates for texture mapping
-      let uvA = uvCoords[a];
-      let uvB = uvCoords[b];
-      let uvC = uvCoords[c];
-
-      // Define the triangle with both position (x, y) and UV texture coordinates
-      vertex(pointA.x, pointA.y, uvA[0], uvA[1]);
-      vertex(pointB.x, pointB.y, uvB[0], uvB[1]);
-      vertex(pointC.x, pointC.y, uvC[0], uvC[1]);
+  if (predictions.length > 0) {
+    const keypoints = predictions[0].scaledMesh;
+    beginShape();
+    for (let i = 0; i < points.length; i++) {
+      const [x, y] = keypoints[points[i]];
+      vertex(x, y);
     }
-
-    endShape();
+    endShape(CLOSE);
   }
 }
